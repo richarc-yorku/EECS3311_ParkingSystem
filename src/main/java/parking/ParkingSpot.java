a0415862f29c57692854c4dfbcd5a9b715637f6d
217761651 <158077739+217761651@users.noreply.github.com>
2025-03-21 16:21:16 -0400

    Update ParkingSpot.java

--- src/main/java/parking/ParkingSpot.java
+++ src/main/java/parking/ParkingSpot.java
@@ -1,26 +1,32 @@
 package parking;
 
-public class ParkingSpot {
-	
-	private int spotID;
-	private Boolean spotStatus;
-	private Sensor sensor;
-	
-	public ParkingSpot(int spotID) {
-		this.spotID = spotID;
-		spotStatus = true;
-		sensor = new Sensor();
-	}
-	
-	public void updateSpot() {
-		//ToDo
-	}
-	
-	public int getID() {
-		return spotID;
-	}
-	
-	public Boolean getStatus() {
-		return spotStatus;
-	}
+public class ParkingSpot extends ParkingLot {
+    private int spotID;
+    private boolean spotStatus;
+    private Sensor sensor;
+
+    public ParkingSpot(int lotID, int spotID) {
+        super(lotID);
+        this.spotID = spotID;
+        this.spotStatus = true;
+        this.sensor = new Sensor();
+    }
+
+    public int getSpotID() {
+        return spotID;
+    }
+
+    public boolean isAvailable() {
+        return spotStatus;
+    }
+
+    public void setAvailable(boolean status) {
+        this.spotStatus = status;
+    }
+
+    public void updateSpot() {
+        this.spotStatus = !sensor.isOccupied(); // not sure if this is enough but have it as this for now
+    }
 }
+
+	// Feel free to change it if I made any glaring mistakes
